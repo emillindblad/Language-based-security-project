14a097f9ccc3f8e37ae5d4ad322d0b8b840e3289
amandapapacosta <amanda.dyrell.papacosta@gmail.com>
2024-04-26 08:35:02 +0200

    idk

--- c-server/server.c
+++ c-server/server.c
@@ -1,10 +1,12 @@
-#include <netinet/in.h>
-#include <sys/types.h>          /* See NOTES */
-#include <sys/socket.h>
-#include <pthread.h>
+#include <asm-generic/socket.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include "unistd.h"
+
 
 #define PORT 8080
 
@@ -21,13 +23,20 @@ int server_connect(int port) {
         exit(0);
     }
 
+    // Avoids "Address in use" error
+    int reuse = -1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+        printf("SO_REUSEPORT failed: %s \n", strerror(errno));
+        return 1;
+    }
+
     // config socket
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(PORT);
 
     // bind to socket
-    if (bind(server_fd, (struct sockaddr*)&server_address, addr_len) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&server_address, addr_len) < 0) {
         perror("bind failed");
         exit(0);
     }
@@ -42,6 +51,10 @@ int server_connect(int port) {
     return server_fd;
 }
 
+void handle_connection() {
+    
+}
+
 int main(int argc, char *argv[]) {
     int port = PORT;
     int server_fd = server_connect(port);
@@ -51,7 +64,7 @@ int main(int argc, char *argv[]) {
         //client info
         struct sockaddr_in client_address;
         socklen_t client_addr_len = sizeof(client_address);
-        int client_fd = accept(server_fd, (struct sockaddr*)&client_address, &client_addr_len);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_address, &client_addr_len);
 
         // accept client connection
         if ((client_fd < 0)) {
@@ -62,10 +75,11 @@ int main(int argc, char *argv[]) {
         printf("connection established. client: %d, port: %i\n", client_fd, PORT);
 
         char buf[256];
-        if (read(client_fd, &buf, sizeof(buf))) {
+        if (read(client_fd, &buf, sizeof(buf)) < 0) {
             perror("connect failed\n");
             exit(0);
         }
+        printf("buffer: %s \n", buf);
         close(client_fd);
         // create new thread to handle new client 
         //pthread_t thread_id;
